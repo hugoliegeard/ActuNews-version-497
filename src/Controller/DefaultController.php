3c0330bcc33071d3dc965a8b4975382310740139
Hugo LIEGEARD <hugo@biyn.media>
2020-10-26 14:16:29 +0100

    Finalisation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,7 +18,20 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('default/index.html.twig');
+        # Récupérer les 6 derniers articles de la BDD ordre décroissant
+        #   ->getRepository(XXX::class) : L'entitée ou je souhaite récupérer les données.
+        #   ->findBy() : Récupère les données selon plusieurs critères
+        #   ->findOneBy() : Récupère un enregistrement selon plusieurs critères
+        #   ->findAll() : Récupère toutes les données de la table
+        #   ->find(id) : Récupère une donnée via son ID
+        $posts = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findBy([], ['id' => 'DESC'], 6);
+
+        # Transmettre à la vue
+        return $this->render('default/index.html.twig', [
+            'posts' => $posts
+        ]);
     }
 
     /**
@@ -34,7 +49,20 @@ class DefaultController extends AbstractController
      */
     public function category($alias)
     {
-        return $this->render('default/category.html.twig');
+        # Récupération de la catégorie via son alias dans l'URL
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['alias' => $alias]);
+        /*
+         * Grâce à la relation entre Post et Category
+         * (OneToMany), je suis en mesure de récupérer
+         * les articles de la catégorie.
+         */
+        $posts = $category->getPosts();
+
+        return $this->render('default/category.html.twig', [
+            'posts' => $posts
+        ]);
     }
 
     /**
@@ -42,10 +70,17 @@ class DefaultController extends AbstractController
      * Permet d'afficher un article du site
      * @Route("/{category}/{alias}_{id}.html", name="default_article", methods={"GET"})
      */
-    public function post($id, $category, $alias)
+    public function post($id)
     {
+        # Récupérer l'article via son ID
+        $post = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->find($id);
+
         # URL : https://localhost:8000/politique/couvre-feu-quand-la-situation-sanitaire-s-ameliorera-t-elle_14155614.html
-        return $this->render('default/post.html.twig');
+        return $this->render('default/post.html.twig', [
+            'post' => $post
+        ]);
     }
 
 
